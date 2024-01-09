25b4f81420c15d21faf1ffe42cc01377329712f1
pvenkatesh <pvenkatesh4287@gmail.com>
2024-01-09 23:26:50 +0530

    shll-robo

--- Mongodb.sh
+++ Mongodb.sh
@@ -29,7 +29,7 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp mongorepo /etc/yum.repos.d/mongorepo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo"
 
