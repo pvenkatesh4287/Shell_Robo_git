a4120ec7e0e7720e4503709a1c255bb0d32e2e8e
pvenkatesh <pvenkatesh4287@gmail.com>
2024-01-10 00:35:11 +0530

    shll-robo

--- catalogue.sh
+++ catalogue.sh
@@ -86,7 +86,7 @@ systemctl start catalogue &>> $LOGFILE
 
 VALIDATE $? "Starting catalogue"
 
-cp /home/centos/Shell_Robo/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/Shell_Roboshop/Mongo.repo /etc/yum.repos.d/Mongo.repo
 
 VALIDATE $? "copying mongodb repo"
 
