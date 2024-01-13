96c413217e20c9a7d9a85a518fe119e3f2bfffaf
pvenkatesh <pvenkatesh4287@gmail.com>
2024-01-13 11:49:05 +0530

    add ll

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-#MONGDB_HOST=mongodb.venkatesh95.online
+MONGDB_HOST=mongodb.venkatesh95.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
@@ -95,6 +95,6 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 VALIDATE $? "Installing MongoDB client"
 
 
-mongo --host $mongodb.venkatesh95.online </app/schema/catalogue.js &>> $LOGFILE
+mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
 
 VALIDATE $? "Loading catalouge data into MongoDB"
